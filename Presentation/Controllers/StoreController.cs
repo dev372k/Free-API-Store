6d262e1cea4992c9161d8117cf2ecf93351ff701
S.M.Owais <smowais.neduet@gmail.com>
2024-04-30 14:38:45 +0500

    initial commit

---
+++ Presentation/Controllers/StoreController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Presentation.Models;
+using System.Diagnostics;
+
+namespace Presentation.Controllers
+{
+    public class StoreController : Controller
+    {
+        private readonly ILogger<StoreController> _logger;
+
+        public StoreController(ILogger<StoreController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
