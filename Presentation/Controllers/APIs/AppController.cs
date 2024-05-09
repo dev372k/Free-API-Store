f6e55935ea9a1a5cecfc958e8b7b3435c15c584e
S.M.Owais <smowais.neduet@gmail.com>
2024-05-09 17:51:00 +0500

    minor fixes

---
+++ Presentation/Controllers/APIs/AppController.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.Models;
+
+namespace Presentation.Controllers.APIs
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppController : ControllerBase
+    {
+        [HttpGet("sentiment")]
+        public IActionResult Sentiment(string text)
+        {
+            try
+            {
+                return Ok(text);
+            }
+            catch (Exception ex)
+            {
+                return Ok(new ResponseModel { Status = false, ErrorMessage = ex.Message, ErrorDetails = ex?.InnerException?.ToString() });
+            }
+        }
+    }
+}
