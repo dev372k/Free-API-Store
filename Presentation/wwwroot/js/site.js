c19cee2c25bed3d81c27f80ace6104af3b93fb0a
S.M.Owais <165829540+dev372k@users.noreply.github.com>
2024-05-12 14:05:04 +0500

    minor bg image changes

--- Presentation/wwwroot/js/site.js
+++ Presentation/wwwroot/js/site.js
@@ -2,8 +2,4 @@
 document.addEventListener("DOMContentLoaded", function () {
     var bgImage = new Image();
     bgImage.loading = "lazy";
-    bgImage.src = "../bg2.jpg";
-    bgImage.onload = function () {
-        document.body.style.backgroundImage = "url('../bg2.jpg')";
-    };
 });
