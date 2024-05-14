36bc513009b538ea35df2dfe2b2292757602b95c
S.M.Owais <smowais.neduet@gmail.com>
2024-05-14 15:45:32 +0500

    Update AuthController.cs

--- Presentation/Controllers/APIs/AuthController.cs
+++ Presentation/Controllers/APIs/AuthController.cs
@@ -2,8 +2,10 @@
 using Application.Abstractions.Interfaces;
 using Application.DTOs;
 using Humanizer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
 using Newtonsoft.Json;
 using Presentation.Models;
 using System.Drawing.Printing;
@@ -19,12 +21,15 @@ public class AuthController : ControllerBase
 {
     private IUserRepo _userRepo;
     private IConfiguration _config;
+    private IStateHelper _stateHelper;
 
     public AuthController(IUserRepo userRepo,
-        IConfiguration config)
+        IConfiguration config,
+        IStateHelper stateHelper)
     {
         _userRepo = userRepo;
         _config = config;
+        _stateHelper = stateHelper;
     }
 
     [HttpPost("login")]
@@ -48,6 +53,28 @@ public class AuthController : ControllerBase
         {
             var res = await _userRepo.Add(request);
             return Ok(new ResponseModel { Message = "User registered successfully.", Data = new { UserId = res } });
+        }
+        catch (Exception ex)
+        {
+            return Ok(new ResponseModel { Status = false, ErrorMessage = ex.Message, ErrorDetails = ex?.InnerException?.ToString() });
+        }
+    }
+
+    [HttpGet("user"), Authorize]
+    public async Task<IActionResult> User()
+    {
+        try
+        {
+            var res = _stateHelper.User();
+            return Ok(new ResponseModel
+            {
+                Data = new
+                {
+                    Id = res.Id,
+                    Name = res.Name,
+                    Email = res.Email          
+                }
+            });
         }
         catch (Exception ex)
         {
